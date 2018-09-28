cd4b6690e640ec6c9144195f12cae11b7307e5d4
juligarcia <juli.garcia.77@gmail.com>
2018-09-27 21:20:43 -0300

    Implementacion del dump, y algunas correcciones de syntax

--- TP1main.py
+++ TP1main.py
@@ -1,59 +1,76 @@
-def crearK(n):
+def crearMatrizK(n):
 
-	"""Crea la matriz K de coeficientes"""
+	"""Crea la matriz K de coeficientes provenientes de la ecuacion diferencial discretizada."""
 
 	if n < 5:
 		return [[0.0]*5 for x in range(5)]
 
-	dim = n+1
+	dimension = n+1
 
-	K = [[0.0]*dim for x in range(dim)]
+	K = [[0.0] * dimension for x in range(dimension)]
 
 	K[0][0] = 1.0
 	K[-1][-1] = 1.0
 
+	#Como notamos un patron en las filas de la matriz K
+	#Adopatmos un vector fijo que se va trasladando y modificando
+
 	vectorFijo = [-4.0, 5.0, -4.0, 1.0]
 
 	for i, p in enumerate(vectorFijo):
+
 		K[1][i] = p
 		K[-2][-i-1] = p
 
 	vectorFijo2 = [1.0] + vectorFijo
 	vectorFijo2[2] += 1
 
-	for i in range(2,n-1):
-		offset = i-2
-		for j,e in enumerate(vectorFijo2):
-			K[i][j+offset] = e
+	for i in range(2, n-1):
+
+		offset = i - 2
+
+		for j, e in enumerate(vectorFijo2):
+
+			K[i][j + offset] = e
 
 	return K
 
-def q(x, g=6.0):
-	return g + g**2 * (x - x**2) 
+def q(x, grupo = 6.0):
 
-def x(n, i, L=1.0):
-	return ((i*L) / n)
+	"""Calcula la carga, donde el parametro "grupo" es estatico."""
 
-def f(n,i,k=1.0,L=1.0):
+	#"grupo" se puede pasar opcionalmente.
+
+	return grupo + grupo ** 2 * (x - x ** 2) 
+
+def x(n, i, L = 1.0):
+
+	"""Calcula el argumento de la funcion "q"."""
+
+	return ((i * L) / n)
+
+def f(n, i, k = 1.0, L = 1.0):
+
 	xi = x(n,i)
 
-	return (q(xi) * (L/n)**4) / k
+	return (q(xi) * (L / n) ** 4) / k
 
 def crearF(n):
 
 	"""Crear matriz F"""
 
-	dim = n+1
+	dimension = n + 1
 
-	F = [0.0]*dim
+	F = [0.0] * dimension
 
 	for i in range(1,n):
+
 		F[i] = f(n,i)
 
 	return F
 
 
-def SOR(K, F, anterior, w=1.0):
+def SOR(K, F, anterior, w = 1.0):
 
 	actualGS = GaussSeidel(K, F, anterior)
 
@@ -71,9 +88,9 @@ def GaussSeidel(K, F, anterior):
 		factor = 0.0
 		for j in range(len(K)):
 			if j != i:
-				factor += (K[i][j]*resultado[j]) / K[i][i]
+				factor += (K[i][j] * resultado[j]) / K[i][i]
 
-		resultado[i] = (F[i]/K[i][i]) - factor
+		resultado[i] = (F[i] / K[i][i]) - factor
 
 	return resultado
 
@@ -89,13 +106,27 @@ def criterioConvergencia(actual, anterior, tolerancia = 0.01):
 
 	normaActual = normaInfinito(actual)
 
-	return normaResta/normaActual <= tolerancia
+	return normaResta / normaActual <= tolerancia
+
+def dumpLista(lista, dumpFile):
+
+	"""dump de los datos de la lista seleccionada, formateado para mejor exportacion"""
+
+	for posicionElemento in range(len(lista)):
+
+		if posicionElemento == 0:
+			dumpFile.write("%.04f" % lista[posicionElemento])
+
+		else:
+			dumpFile.write("	" + "%.04f" % lista[posicionElemento])
+
+	dumpFile.write("\n")
+
 
 def main():
 
 	tomarIntervalos = True
 	intervalos = []
-	iteracionesTotales = 1
 
 	while tomarIntervalos:
 
@@ -110,30 +141,44 @@ def main():
 	if intervalos:
 		print("Se ejecutara el programa para los siguientes intervalos: {} ".format(intervalos))
 
-	w = float(input("Ingrese el factor de relajacion: "))
+	#w = float(input("Ingrese el factor de relajacion: "))
+
+	factoresDeRelajacion = [x / 100 for x in range(100, 200, 5)]
 
 	for n in intervalos:
 
-		dim = n + 1
+		dumpFile = open("Intervalos_" + str(n) + ".txt", "w")
 
-		anterior = [0] * dim
+		for factorDeRelajacion in factoresDeRelajacion:
 
-		K = crearK(n)
+			dimension = n + 1
 
-		F = crearF(n)
+			anterior = [0] * dimension
 
-		actual = SOR(K, F, anterior, w)
+			K = crearMatrizK(n)
 
-		while not (criterioConvergencia(actual, anterior)):
+			F = crearF(n)
 
-			anterior = actual
-			actual = SOR(K, F, anterior, w)
+			actual = SOR(K, F, anterior, factorDeRelajacion)
 
-			print(actual)
+			iteracionesTotales = 1
 
-			iteracionesTotales += 1
+			dumpFile.write("Procesamiento del SEL con factor de relajacion = " + str(factorDeRelajacion) + "\n\n")
+			dumpLista(actual, dumpFile)
 
-		print(iteracionesTotales)
+			while not (criterioConvergencia(actual, anterior)):
+
+				anterior = actual
+				actual = SOR(K, F, anterior, factorDeRelajacion)
+
+				dumpLista(actual, dumpFile)
+
+				iteracionesTotales += 1
+
+			dumpFile.write("\n" + "Iteraciones totales: " + str(iteracionesTotales) + "\n\n")
+			dumpFile.write("----------------------------------------------------- \n\n")
+
+		dumpFile.close()
 
 
 """K = [[10.0,2.0,6.0], [1.0,10.0,4.0], [2.0,-7.0,-10.0]]
